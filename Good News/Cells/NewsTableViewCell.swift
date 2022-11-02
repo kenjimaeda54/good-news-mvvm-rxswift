a53e03d14f2071ba68f4ba30df1535d1495dcfeb
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-11-01 23:04:47 -0300

    feat: finished project

---
+++ Good News/Cells/NewsTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  NewsTableViewCell.swift
+//  Good News
+//
+//  Created by kenjimaeda on 01/11/22.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+	 
+	@IBOutlet weak var labTitleNew: UILabel!
+	@IBOutlet weak var labDescriptionNew: UILabel!
+	
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+}
--- Good News/Controllers/NewsTableViewController.swift
+++ Good News/Controllers/NewsTableViewController.swift
@@ -13,6 +13,7 @@ class NewsTableViewController: UITableViewController {
 	
 	//MARK: - Vars
 	let disposed = DisposeBag()
+	var articleVM: ArticlesListViewModel!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -27,10 +28,45 @@ class NewsTableViewController: UITableViewController {
 		
 		let resource = Resource<ArticleResult>(url: url)
 		
-		URLRequest.load(resource).subscribe(onNext:{
-			 print($0)
+		URLRequest.load(resource).subscribe(onNext:{[self] response in
+			 
+			let articles = response.articles
+			articleVM = ArticlesListViewModel(articles)
+			
+			DispatchQueue.main.async {
+				self.tableView.reloadData()
+			}
+			 
 		}).disposed(by: disposed)
 		
 	}
-
+	 
+	override func numberOfSections(in tableView: UITableView) -> Int {
+		 return 1
+	}
+	 
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return articleVM != nil  ? articleVM.articlesVM.count : 0
+	}
+ 
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "cellGodNews", for: indexPath) as! NewsTableViewCell
+		
+		let article = articleVM.articleAt(indexPath.row)
+		 
+		article.title.asDriver(onErrorJustReturn: "")
+			.drive(
+				cell.labTitleNew.rx.text
+			).disposed(by: disposed)
+		
+		article.description.asDriver(onErrorJustReturn: "")
+			.drive(
+				cell.labDescriptionNew.rx.text
+			).disposed(by: disposed)
+		
+		return cell
+		
+	}
+	 
+	
 }
